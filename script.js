6f3dd0f92370cfb6f9148235808a84ebab2c3bf7
Suhaila <suben@microsoft.com>
2021-01-15 04:48:08 -0800

    established some style

---
+++ script.js
@@ -0,0 +1,9 @@
+let myLibrary = [];
+let palette = ['f94144', 'f3722c', 'f8961e', 'f9844a', 'f9c74f', '90be6d', '43aa8b', '4d908e', '577590', '277da1'];
+
+function Book() {
+}
+
+function addBookToLibrary() {
+
+}
