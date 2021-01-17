7d290d53b84790e62cd9a2d8d3ddade6aa477fc2
Suhaila <suben@microsoft.com>
2021-01-16 19:33:42 -0800

    form does the thing now

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ let myLibrary = [];
 let palette = ['f94144', 'f3722c', 'f8961e', 'f9844a', 'f9c74f', '90be6d', '43aa8b', '4d908e', '577590', '277da1'];
 
 class Book {
-    constructor(title, author, pages, read) {
+    constructor(title, author, pages, read, genre) {
         this.title = title;
         this.author = author;
         this.pages = pages;
         this.read = read;
+        this.genre = genre;
         this.color = palette[Math.floor(Math.random() * 10)];
     }
     readTheBook() {
@@ -22,7 +23,7 @@ function addBookToLibrary(yourBook) {
     myLibrary.push(yourBook);
 }
 
-function updateDisplay() {
+function updateDisplayInit() {
     const collection = document.querySelector(".collection");
 
     for (let element of myLibrary) {
@@ -34,13 +35,13 @@ function updateDisplay() {
     }
 }
 
-let hP = new Book('Harry Potter', 'Rowling', 300, true);
-let lOTR = new Book('Lord of the Rings', 'Tolkien', 400, false);
+let hP = new Book('Harry Potter', 'Rowling', 300, true, 'Fantasy');
+let lOTR = new Book('Lord of the Rings', 'Tolkien', 400, false, 'Fantasy');
 
 addBookToLibrary(hP);
 addBookToLibrary(lOTR);
 
-updateDisplay()
+updateDisplayInit()
 
 function toggleForm() {
     let formy = document.getElementById('theForm');
@@ -48,3 +49,27 @@ function toggleForm() {
 }
 
 document.getElementById('theButton').addEventListener('click', toggleForm);
+
+const theForm = document.querySelector('form')
+theForm.onsubmit = function() {
+    event.preventDefault()
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let pages = parseInt(document.getElementById('pages').value);
+    let genre = document.getElementById('genre').value;
+    let read = document.getElementById('read').value;
+
+    let userBook = new Book(title, author, pages, genre, read);
+    addBookToLibrary(userBook);
+    updateDisplay();
+
+    function updateDisplay() {
+        const collection = document.querySelector(".collection");
+        let node = document.createElement('div');
+        node.style.backgroundColor = `#${userBook.color}`;
+        let content = document.createTextNode(`${userBook.title} by ${userBook.author}`)
+        node.appendChild(content);
+        collection.appendChild(node);
+    }
+}
+
